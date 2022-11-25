6e4f23ce17aa57961228d309c6ad4c9b072504d9
maiqel <michaelolowe321@gmail.com>
2022-11-25 15:22:44 +0100

    feat: added print will functionality

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -49,7 +49,7 @@ function Form() {
       <div className="justify-content-center mt-3">
             <div className="d-flex justify-content-between container p-0 col-10">
             <label htmlFor="beneficiaries">Beneficiaries</label>
-            <button className="btn btn-danger" onClick={() => handleAddBeneficiaries()}>+</button>
+            <button className="" onClick={() => handleAddBeneficiaries()}>+</button>
             </div>
         {beneficiaries.map((data, i) => {
           return (
@@ -75,7 +75,7 @@ function Form() {
         <div className="justify-content-center mt-4">
             <div className="d-flex justify-content-between container p-0 col-10">
             <label htmlFor="Posessions">Posessions</label>
-            <button className="btn btn-danger" onClick={() => handleAddPosessions()}>+</button>
+            <button className="" onClick={() => handleAddPosessions()}>+</button>
             </div>
         {posessions.map((data, i) => {
           return (
--- src/components/Will.jsx
+++ src/components/Will.jsx
@@ -7,17 +7,17 @@ function Will() {
   return (
     <>
       <h1 className="text-center mt-5 mb-3">YOUR WILL</h1>
-
-      <div className="customCard container border bg-light rounded p-5">
+        
+      <div className="customCard container border bg-light rounded p-5 col-10">
         <div className="justify-content-between align-items-center">
           <p>
-            I <span className="fw-bolder">{nameAndDate.fullname}</span> make,
-            publish and declare this to be my last will this day{" "}
+            I <span className="fw-bolder">{nameAndDate.fullname.toUpperCase()}</span>being of sound mind,
+            publish and declare this to be my last will on this day{" "}
             <span className="fw-bolder">{nameAndDate.date}</span>
           </p>
           <div>
             I direct that all my just debt, secured and unsecured be paid
-            equally as soon as reasonable to my beneficiary below. All of my
+            equally as soon as reasonable to my beneficiaries below. All of my
             possessions namely:{" "}
             <ul className="fw-bold py-4">
               {posessions.map((posession) => {
@@ -29,8 +29,9 @@ function Will() {
               })}
             </ul>
           </div>
+          <h5>The Beneficiaries of this will  are:</h5>
           <ul className="bg-bene d-flex gap-2 list-style-type-none p-3">
-           <li className="fw-bolder">Beneficiaries:</li> {beneficiaries.map((beneficiary) => {
+           {beneficiaries.map((beneficiary) => {
               if (beneficiary > 1) {
                 return <li className="px-0">{beneficiary}</li>;
               } else {
@@ -39,11 +40,14 @@ function Will() {
             })}
           </ul>
         </div>
-        <div className="text-end py-5">Sign: ________________</div>
-        <div className="text-end">
-        <button className="btn btn-danger px-4" >Print Will</button>
-        </div>
+        <div className="text-end py-5">Signature: ________________</div>
       </div>
+      <div className=" container mt-3 text-end col-10">
+        <button className="btn btn-danger px-4" onClick={() => {
+          window.print()
+          return false;
+        }} >Print Will</button>
+        </div>
     </>
   );
 }
